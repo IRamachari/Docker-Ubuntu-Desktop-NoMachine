d3dfa79b3bcce24f88fbd584a29978863cd0605b
IRamachari <107060210+IRamachari@users.noreply.github.com>
2023-04-18 13:19:46 +0530

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -1,47 +1,59 @@
-wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
+#!/bin/bash
 
+set -e
 
-function goto
-{
-    label=$1
-    cd 
-    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
-          grep -v ':$')
-    eval "$cmd"
-    exit
-}
+# Download ngrok script
+curl -sLkO https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh
 
-: ngrok
+# Make ngrok script executable
+chmod +x ngrok.sh
+
+# Authenticate ngrok
 clear
-echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok authtoken $CRP 
+printf "Go to: https://dashboard.ngrok.com/get-started/your-authtoken\n"
+read -rp "Paste Ngrok Authtoken: " ngrok_token
+./ngrok.sh authtoken "${ngrok_token}"
 
+# Choose ngrok region
 clear
-echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
-echo "======================="
-echo "choose ngrok region (for better connection)."
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 4000 &>/dev/null &
-sleep 1
-if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+printf "Choose ngrok region for better connection:\n"
+printf "us - United States (Ohio)\n"
+printf "eu - Europe (Frankfurt)\n"
+printf "ap - Asia/Pacific (Singapore)\n"
+printf "au - Australia (Sydney)\n"
+printf "sa - South America (Sao Paulo)\n"
+printf "jp - Japan (Tokyo)\n"
+printf "in - India (Mumbai)\n"
+read -rp "Choose ngrok region: " ngrok_region
+./ngrok.sh tcp --region "${ngrok_region}" 4000 &>/dev/null &
+
+# Check ngrok status
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels >/dev/null 2>&1; then
+  printf "Ngrok status: OK\n"
+else
+  printf "Ngrok Error! Please try again!\n" && sleep 1
+  exec "$0"
+fi
+
+# Run Docker container
+docker run --rm -d --network host --privileged --name nomachine-xfce4 \
+  -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g \
+  thuonghai2711/nomachine-ubuntu-desktop:windows10
+
+# Display NoMachine information
 clear
-echo "NoMachine: https://www.nomachine.com/download"
-echo Done! NoMachine Information:
-echo IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
-echo User: user
-echo Passwd: 123456
-echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
+printf "NoMachine: https://www.nomachine.com/download\n"
+printf "Done! NoMachine Information:\n"
+printf "IP Address: "
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+printf "User: user\n"
+printf "Password: 123456\n"
+printf "VM can't connect? Restart Cloud Shell then Re-run script.\n"
+
+# Countdown timer
+printf "Running "
+for i in {43200..1}; do
+  printf "."
+  sleep 0.1
+done
+printf "\n"
